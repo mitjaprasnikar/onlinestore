9ebc62977283bb45604d268e9a57ef93b2b9b499
mitjaprasnikar <mitja.prasnikar@gmail.com>
2020-03-18 14:39:49 +0100

    header and link components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import Shop from "../src/pages/shop/shop"
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={Shop}/>
